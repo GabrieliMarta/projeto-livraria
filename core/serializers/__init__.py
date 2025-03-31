5f4ec501b631499d899b6346ac57b881ef1b8804
gabrielimarta <gabrielirocha2730@gmail.com>
2025-03-31 16:53:43 -0300

    criação de uma API REST

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,1 +1,2 @@
-from .user import UserSerializer
+from .user import User
+from .categoria import Categoria
---
+++ core/serializers/categoria.py
@@ -0,0 +1,9 @@
+from attrs import field
+from pydantic import model_serializer
+from rest_framework.serializers import ModelSerializer
+from core.models import Categoria, Categorias
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = " __all__ "
